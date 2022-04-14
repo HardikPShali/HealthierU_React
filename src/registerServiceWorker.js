dda89a1ffeb22b95b2a9d5aa025bac92fc0944dd
Hardik Shali <hardik.s@accubits.com>
2022-04-14 16:05:46 +0530

    Removed window.location from Doctor,Login module and all the other files

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,7 +1,5 @@
 // In production, we register a service worker to serve assets from local cache.
 
-import { useHistory } from "react-router";
-
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on the "N+1" visit to a page, since previously
@@ -9,7 +7,7 @@ import { useHistory } from "react-router";
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
-const history = useHistory();
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -21,7 +19,6 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
- 
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
@@ -96,7 +93,7 @@ function checkValidServiceWorker(swUrl) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
-            history.go(0);
+            window.location.reload();
           });
         });
       } else {
