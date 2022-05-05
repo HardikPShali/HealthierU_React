92388df56916e5c010a102f5f5b1fdb4e9df571e
hridayanp <hridayanphukan123@gmail.com>
2022-05-05 20:12:58 +0530

    added toast container in questionnaire.

--- src/components/Patient Module/QuestionnaireNew/Questionnaire.js
+++ src/components/Patient Module/QuestionnaireNew/Questionnaire.js
@@ -4,10 +4,29 @@ import { Button, Col, Container, Row } from 'react-bootstrap';
 import quesJson from './dummyQuestions.json';
 import { Questions } from './Questions';
 import '../patient.css';
+import { useHistory } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Questionnaire = () => {
     const [questions, setQuestions] = useState(null);
-    // const [loading]
+    const [loading, setLoading] = useState(true);
+
+    const history = useHistory();
+
+
+    const onContinue = async (event) => {
+        toast.success('Good Job!', {
+            position: 'top-right',
+            autoClose: 2000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+        setTimeout(() => history.push('/patient'), 1000);
+    }
 
     useEffect(() => {
         setQuestions(quesJson);
@@ -34,10 +53,21 @@ const Questionnaire = () => {
                             type="submit"
                             variant="primary"
                             className="Questionnaire-Continue-Button"
-                            onClick={(e) => console.log(questions)}
+                            onClick={onContinue}
                         >
                             Continue
                         </Button>
+                        <ToastContainer
+                            position="top-right"
+                            autoClose={5000}
+                            hideProgressBar
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                        />
                     </div>
                 </Col>
 
