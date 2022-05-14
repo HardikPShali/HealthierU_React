7486854499e28dc10cfeeb1f6b1c77a4b5c0a348
Nithi <58935041+imNithi@users.noreply.github.com>
2022-05-14 11:26:55 +0530

    upcoming appointment design change

--- src/components/Patient Module/UpcomingAppointmentsSection/UpcomingAppointmentCard.js
+++ src/components/Patient Module/UpcomingAppointmentsSection/UpcomingAppointmentCard.js
@@ -1,36 +1,67 @@
-import React from 'react'
-import home2 from '../../../images/home-2.png'
-import moment from 'moment';
-
+import React, { useEffect, useState } from "react";
+import home2 from "../../../images/home-2.png";
+import moment from "moment";
+import calendarSmall from "../../../images/svg/calendar-small.svg";
+import timeSmall from "../../../images/svg/time-small.svg";
+import useRole from "../../../custom-hooks/useRole";
+import { ROLES } from "../../../util/configurations";
 
 const UpcomingAppointmentCard = ({ appointment }) => {
+  const [appointmentPersonKey, setAppointmentPersonKey] = useState("");
+  const [roles] = useRole();
 
+  useEffect(() => {
+    const key = roles.some((role) => role === ROLES.ROLE_PATIENT)
+      ? "doctor"
+      : "patient";
+    setAppointmentPersonKey(key);
+  }, []);
 
-    return (
-        // console.log('UA', appointments)
-        < div className='row' >
-            {/* {console.log('UA', appointment)} */}
-            <div className='col-md-4'>
-                <div className='upcoming-appointment-card__img-wrapper'>
-                    <img src={home2} alt="nutrition" className='img-circle' />
-                </div>
+  return (
+    // console.log('UA', appointments)
+    <div className="row align-items-start">
+      {/* {console.log('UA', appointment)} */}
+      <div className="col-md-3">
+        <img src={home2} alt="nutrition" className="img-circle ml-3 mt-3" />
+      </div>
+      <div className="col-md-9">
+        <div className="upcoming-appointment-card__card-details">
+          <h5 className="upcoming-appointment-card__doctor-name">
+            {appointment[appointmentPersonKey] &&
+              appointment[appointmentPersonKey].firstName +
+                " " +
+                appointment[appointmentPersonKey].lastName}
+          </h5>
+          <span className="upcoming-appointment-card__specality">
+            {appointmentPersonKey === "doctor" &&
+              appointment[appointmentPersonKey] &&
+              appointment[appointmentPersonKey].specialities.length &&
+              appointment[appointmentPersonKey].specialities[0].name}
+
+            {appointmentPersonKey === "patient" &&
+              appointment.unifiedAppointment &&
+              appointment.unifiedAppointment.split(
+                "#"
+              )[1].replace("_", " ")}
+          </span>
+          <div className="upcoming-appointment-card__card-details--date-div">
+            <div class="upcoming-appointment-card__card-time-row">
+              <img src={calendarSmall} />
+              <span className="upcoming-appointment-card__common-span">
+                {moment(appointment.startTime).format("DD/MM/YY")}
+              </span>
             </div>
-            <div className='col-md-8'>
-                <div className='upcoming-appointment-card__card-details'>
-                    <h5 className='upcoming-appointment-card__doctor-name'>{appointment.doctor.firstName}</h5>
-                    <span className='upcoming-appointment-card__common-span'>
-                        {appointment.doctor.specialities.length && appointment.doctor.specialities[0].name}
-                    </span>
-                    <div className='upcoming-appointment-card__card-details--date-div'>
-                        <span className='upcoming-appointment-card__common-span'>{moment(appointment.startTime).format("DD/MM/YY")}</span>
-                        <span className='upcoming-appointment-card__common-span'>{moment(appointment.startTime).format("hh:mm A")}</span>
-                    </div>
-                </div>
+            <div class="upcoming-appointment-card__card-time-row ml-4">
+              <img src={timeSmall} />
+              <span className="upcoming-appointment-card__common-span">
+                {moment(appointment.startTime).format("hh:mm A")}
+              </span>
             </div>
-        </div >
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-    )
-}
-
-export default UpcomingAppointmentCard
+export default UpcomingAppointmentCard;
