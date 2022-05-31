1e0b979fbbd24a6ca387159afbce667db93c0696
masanamuthu <91948630+Masanamuthu77@users.noreply.github.com>
2022-05-31 19:28:18 +0530

    Navbar links added for web and navigated to respective page, Black color fonts replaced with #3E4543, Our Edge-Image replaced, Powerful Features-Image replaced, Footer font colors replaced with #3E4543, Mobile navigation navbar working in progress.

--- src/components/Login-Module/Header.js
+++ src/components/Login-Module/Header.js
@@ -1,30 +1,56 @@
-import React, { Component } from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import {NavLink} from 'react-router-dom'
-import logo from '../../images/logo/logo_white.svg';
-import './landing.css'
+import React, { Component } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import logo from "../../images/logo/logo_white.svg";
+import "./landing.css";
+import { HashLink as Link } from "react-router-hash-link";
 // import { Container } from 'react-bootstrap';
 //import SearchIcon from '@material-ui/icons/Search';
 
 export const Header = ({ hideButton }) => {
-    return (
-        <Navbar variant="dark" id="navbar">
-            <Container>
-            <NavLink to="/" className="mr-auto">
-               <img
-                    src={logo}
-                    id="icon"
-                    alt="HealthierU Logo"
-                    style={{ width: "70%" }}
-                />
-            </NavLink>
-            <Nav>
-                {!hideButton && <NavLink to="/signin"><button className="btn btn-secondary">Sign in / Join Now</button></NavLink>}
-            </Nav>
-        </Container>
-        </Navbar>
-    )
-}
+    
 
+  return (
+    <Navbar variant="dark" id="navbar">
+      <Container className="web_navigation">
+        <NavLink to="/" className="mr-auto">
+          <img
+            src={logo}
+            id="icon"
+            alt="HealthierU Logo"
+            style={{ width: "70%" }}
+          />
+        </NavLink>
+        <Nav>
+          {!hideButton && (
+            <div className="nav-menu-wrapper">
+              <div className="nav-menu-container">
+                <ul className="nav-menu-ul">
+                  <li>
+                    <Link to="/#about-us">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/#how-it-work">How It Works</Link>
+                  </li>
+                  <li>
+                    <Link to="/#our-service">Our Services</Link>
+                  </li>
+                  <li>
+                    <Link to="/#footer">Contact Us</Link>
+                  </li>
+                </ul>
+              </div>
+              <NavLink to="/signin">
+                <button className="btn btn-secondary">
+                  Sign in / Join Now
+                </button>
+              </NavLink>
+            </div>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
