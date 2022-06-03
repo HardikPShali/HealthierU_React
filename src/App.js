1092800c14e55a883b879a043425204869c3eb56
hridayanp <hridayanphukan123@gmail.com>
2022-06-03 13:06:02 +0530

    changed client_id for paypal in App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ const App = () => {
       <PayPalScriptProvider
         options={{
           'client-id':
-            'AcH8aU79kD2tItN2vETkGYHZAnbiCAOwTNvNpVTUPSctfTiCSTLzgyXtXQNgIchyniPoP0D9Lg7vxP7N',
+            'AVQsrQWNO79AZmzitgrSy2bPd_r8YqYLOKVIGTkcWxbl6_oDKI-rQzDa60LBSYn7XOHStiiWy_u7bhJQ',
           'currency': 'USD',
           'intent': 'capture',
         }}
