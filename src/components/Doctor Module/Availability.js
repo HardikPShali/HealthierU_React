3eda6eb92c2e1a74f352221c886aee4e6ad22ae9
masanamuthu <91948630+Masanamuthu77@users.noreply.github.com>
2022-06-14 12:41:02 +0530

    conditionally showing time section added.

--- src/components/Doctor Module/Availability.js
+++ src/components/Doctor Module/Availability.js
@@ -13,6 +13,7 @@ const Availability = () => {
     setSelectedTimeSlot(value);
     console.log(selectedTimeSlot);
     tempDays = [];
+
   };
 
   const handleValue = (value) => {
@@ -66,98 +67,104 @@ const Availability = () => {
 
     setTimes(newTimes);
     console.log(times);
+
   };
 
   return (
     <Container>
       <div className="slot-time available-btn">
         <h3>Set Availability</h3>
-        <p>There is no time slots. Please add your time slots.</p>
-        <button>Set Time Slots</button>
+        {times.length ? (
+          <></>
+        ) : (
+          <p>There is no time slots. Please add your time slots.</p>
+        )}
       </div>
-      {value && (
-        <Row className="time-slot-container">
-          <Col sm={12} md={6} lg={6} xSl={6}>
-            <div className="slot-time-wrap">
-              <div className="start-time-wrap">
-                <h5>Set Time</h5>
-                <TimeRangePicker
-                  className=" mt-1 mb-3"
-                  amPmAriaLabel="Select AM/PM"
-                  value={value}
-                  onChange={handleValue}
-                />
-              </div>
-              <div className="available-btn">
-                <button onClick={saveTimeHandler}>Set Time</button>
-              </div>
-            </div>
-            {value && (
-              <div className="days-wrapper">
-                {allDays.map((dayValue) => (
-                  <label className="day-wrapper">
-                    {dayValue}
-                    <input
-                      type="checkbox"
-                      onChange={dayHandler}
-                      value={dayValue}
-                    />
-                    <span class="checkmark"></span>
-                  </label>
-                ))}
-                <div className="available-btn">
-                  <button onClick={addDaySlot}>Add Time Slots</button>
-                </div>
-              </div>
-            )}
-          </Col>
-          <Col sm={12} md={6} lg={6} xl={6}>
-            <div className="selected-time-container">
-              <h5>Select Time Slots</h5>
 
-              {times.map((timeData, timeIndex) => (
-                <div
-                  onClick={() => getSelectedTimeDtls(timeIndex)}
-                  className="selected_time"
-                  key={timeData.id}
-                >
-                  <div className="select-time-wrap">
-                    <div className="select-time-font">
-                      <h6 className="select-time-font">{timeData.from}</h6>
-                      <h6 className="select-time-font pl-2 pr-2">to</h6>
-                      <h6 className="select-time-font">{timeData.to}</h6>
-                    </div>
-                  </div>
-                  <div className="close-btn-select">
-                    <img src={closeBtn} alt="close button" />
+      <Row className="time-slot-container">
+        <Col sm={12} md={6} lg={6} xSl={6}>
+          <div className="slot-time-wrap">
+            <div className="start-time-wrap">
+              <h5>Set Time</h5>
+              <TimeRangePicker
+                className=" mt-1 mb-3"
+                amPmAriaLabel="Select AM/PM"
+                value={value}
+                onChange={handleValue}
+              />
+            </div>
+            <div className="available-btn">
+              <button onClick={saveTimeHandler}>Set Time</button>
+            </div>
+          </div>
+          {times.length ? (
+            <div className="days-wrapper">
+              {allDays.map((dayValue) => (
+                <label className="day-wrapper">
+                  {dayValue}
+                  <input
+                    type="checkbox"
+                    onChange={dayHandler}
+                    value={dayValue}
+                  />
+                  <span class="checkmark"></span>
+                </label>
+              ))}
+              <div className="available-btn">
+                <button onClick={addDaySlot}>Add Time Slots</button>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+        </Col>
+        <Col sm={12} md={6} lg={6} xl={6}>
+          <div className="selected-time-container">
+            <h5>Select Time Slots</h5>
+            {times.map((timeData, timeIndex) => (
+              <div
+                onClick={() => getSelectedTimeDtls(timeIndex)}
+                className="selected_time"
+                key={timeData.id}
+              >
+                <div className="select-time-wrap">
+                  <div className="select-time-font">
+                    <h6 className="select-time-font">{timeData.from}</h6>
+                    <h6 className="select-time-font pl-2 pr-2">to</h6>
+                    <h6 className="select-time-font">{timeData.to}</h6>
                   </div>
                 </div>
-              ))}
-            </div>
-            <div className="selected-day-container">
-              <h5>Select Time with Day Slots</h5>
-              {times.map((eachTimes) =>
-                eachTimes.days.length ? (
-                  <div className="selected-day-wrap">
-                    <div className="selected-days">
-                      <h5>{eachTimes.days.toString()}</h5>
-                      <span className="mr-3">{eachTimes.from} to {eachTimes.to}</span>
-                    </div>
-                    <div className="selected-days-toggle">
-                      <label class="switch">
-                        <input type="checkbox" />
-                        <span class="slider round"></span>
-                      </label>
-                    </div>
+                <div className="close-btn-select">
+                  <img src={closeBtn} alt="close button" />
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="selected-day-container">
+            <h5>Select Time with Day Slots</h5>
+            {times.map((eachTimes) =>
+              eachTimes.days.length ? (
+                <div className="selected-day-wrap">
+                  <div className="selected-days">
+                    <h5>{eachTimes.days.toString()}</h5>
+                    <span className="mr-3">
+                      {eachTimes.from} to {eachTimes.to}
+                    </span>
                   </div>
-                ) : (
-                  <> </>
-                )
-              )}
-            </div>
-          </Col>
-        </Row>
-      )}
+                  <div className="selected-days-toggle">
+                    <label class="switch">
+                      <input type="checkbox" />
+                      <span class="slider round"></span>
+                    </label>
+                  </div>
+                </div>
+              ) : (
+                <> </>
+              )
+            )}
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
