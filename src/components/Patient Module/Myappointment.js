5458133b3f5664ee5d3d3b7ece4e4a02d650c5d0
hardik.s2 <hardik.s@accubits.com>
2022-07-05 13:40:55 +0530

    added right path for reschedule

--- src/components/Patient Module/Myappointment.js
+++ src/components/Patient Module/Myappointment.js
@@ -1205,9 +1205,7 @@ const Myappointment = (props) => {
                     <div className="details-links">
                       <Link
                         to={{
-                          pathname: `/patient/rescheduleappointment/${selectedAppointment.id}/${selectedAppointment.appointmentMode?.toLowerCase().replace(" ", "-")}`,
-
-
+                          pathname: `/patient/rescheduleappointment/${selectedAppointment.id}/${selectedAppointment.appointmentMode?.toLowerCase().replace(" ", "-")}/${selectedAppointment.unifiedAppointment}`,
                         }}
                       >
                         <div style={{ display: 'flex', alignItem: 'center' }}>
