6dac78de00d58d132373ad10ea72bab536a18ed2
Nithi <58935041+imNithi@users.noreply.github.com>
2022-07-23 12:07:14 +0530

    chat and call notification trigger from other pages

--- src/components/CommonModule/Chat/ChatPage/ChatItem.js
+++ src/components/CommonModule/Chat/ChatPage/ChatItem.js
@@ -42,7 +42,7 @@ const ChatItem = ({ item, onChatChange, messageDateFormat, selectedChatItem }) =
             {/* <img src={item[item.userKey]?.picture} alt="" /> */}
           </div>
           <div className="chat_item-text-wrap">
-            <div className="chat_item-name">{item[item.userKey]?.firstName} {item[item.userKey]?.lastName}</div>
+            <div className="chat_item-name">{item[item.userKey]?.firstName}</div>
             <div className="chat_item-message-wrap">
               {item.lastMessage?.isMyMessage && <span className="chat_item-by">You</span>}
               <span className="chat_lastMessage">{item.lastMessage?.message || ""}</span>
