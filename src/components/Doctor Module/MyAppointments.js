52574184e5cae02779ec01b1b16c60c74f37db3f
hridayanp <hridayanphukan123@gmail.com>
2022-07-27 10:54:05 +0530

    chnaged label as instructed | HD_002

--- src/components/Doctor Module/MyAppointments.js
+++ src/components/Doctor Module/MyAppointments.js
@@ -1063,7 +1063,7 @@ const MyAppointments = (props) => {
           id="customized-dialog-title"
           onClose={handleRescheduleClose}
         >
-          Are you sure you want to Reschedule this patient's slot?
+          Are you sure you want to reschedule this patient's slot?
         </DialogTitle>
         <DialogActions>
           <button
